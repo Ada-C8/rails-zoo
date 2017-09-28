39faf0958d383a6d6a91d52dc15e32e776d7c496
Jessica Owens <vertige@gmail.com>
2017-09-28 16:16:09 -0700

    Created Aminals#show route, controller method and view

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,4 +3,10 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
+
+  def show
+    @animal = Animal.find_by(id: params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "animals#index"
 
   get "animals", to: "animals#index"
 
-
+  get "animals/:id", to: "animals#show", as: "show"
 
 end
